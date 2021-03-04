d94446cb345b75cfa4e267b85a5217fe58932e3b
Pabs Monestel <pablo.monestel@gmail.com>
2021-03-04 09:44:01 -0600

    Se crean los archivos para las rutinas

---
+++ routes/rutinas.route.js
@@ -0,0 +1,34 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const Rutina = require('../models/rutinas.model');
+
+router.post('/registrar-rutina', (req, res) => {
+    let rutina = JSON.parse(req.body.obj);
+
+    let nueva_rutina = new Rutina({
+        creacion: rutina.creacion,
+        vencimiento: rutina.vencimiento
+    });
+
+    rutina.lista_ejercicios.forEach(ejercicio => {
+        nueva_rutina.ejercicios.push(ejercicio);
+    });
+
+    nueva_rutina.save((err, rutina) => {
+        if (err) {
+            res.json({
+                msj: 'La rutina no se pudo registrar',
+                err
+            });
+        } else {
+            res.json({
+                msj: 'La rutina se registró correctamente',
+                rutina
+            });
+        }
+    });
+});
+
+
+module.exports = router;
